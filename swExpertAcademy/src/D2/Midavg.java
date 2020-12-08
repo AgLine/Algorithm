94f06eda63b24bacc45d8047a234e1e852e9fb4e
AgLine <rladmstjs120@gmail.com>
2020-12-08 15:05:25 +0900

    1984. 중간 평균값 구하기

--- swExpertAcademy/src/D2/Midavg.java
+++ swExpertAcademy/src/D2/Midavg.java
@@ -7,18 +7,18 @@ public class Midavg {
 
     public static void main(String[] args) throws Exception{
         int a[] = new int [10];
-            int sum=0;
-            int avg=0;
-            
-            for(int i=0;i<10;i++){
-            	a[i]= sc.nextInt();
-            }
-            Arrays.sort(a);
-            
-            for(int i=1;i<9;i++){
-            	sum+=a[i];
-            }
-            avg = (int)Math.round(sum/8.0);
-            System.out.println(avg);
+        int sum=0;
+        int avg=0;
+        
+        for(int i=0;i<10;i++){
+            a[i]= sc.nextInt();
+        }
+        Arrays.sort(a);
+        
+        for(int i=1;i<9;i++){
+            sum+=a[i];
+        }
+        avg = (int)Math.round(sum/8.0);
+        System.out.println(avg);
     }
 }
